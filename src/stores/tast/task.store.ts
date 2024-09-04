13136eed71e64193d0d7c0ef904942367bac9d41
BrunoAlan <alan.brunoab@gmail.com>
2024-09-04 19:37:44 -0300

    refactor: Update SingleTask component to use task store for dragging functionality
    The code changes in `SingleTask.tsx` refactor the `SingleTask` component to use the `useTaskStore` hook from the `task.store` for managing the dragging functionality. This improves code organization and maintainability by centralizing the dragging logic in the store.

--- src/stores/tast/task.store.ts
+++ src/stores/tast/task.store.ts
@@ -1,13 +1,22 @@
+import { devtools } from 'zustand/middleware';
 import type { Task, TaskStatus } from '../../interfaces';
 import { create, StateCreator } from 'zustand';
 
 interface TaskState {
+    draggingTaskId?: string;
     tasks: Record<string, Task>; // same as => tasks: { [key: string]: Task };
 
     getTaskByStatus: (status: TaskStatus) => Task[];
+
+    setDraggingTaskId: (id: string) => void;
+    removeDraggingTaskId: () => void;
 }
 
-const storeApi: StateCreator<TaskState> = (set, get) => ({
+const storeApi: StateCreator<TaskState, [['zustand/devtools', never]]> = (
+    set,
+    get
+) => ({
+    draggingTaskId: undefined,
     tasks: {
         '1': {
             id: '1',
@@ -37,6 +46,14 @@ const storeApi: StateCreator<TaskState> = (set, get) => ({
             (task: Task) => task.status === status
         );
     },
+
+    setDraggingTaskId(taskId: string) {
+        console.log(taskId);
+        set({ draggingTaskId: taskId });
+    },
+    removeDraggingTaskId() {
+        set({ draggingTaskId: undefined });
+    },
 });
 
-export const useTaskStore = create<TaskState>()(storeApi);
+export const useTaskStore = create<TaskState>()(devtools(storeApi));
