50e74341284299b74160f7e392a9b06ba14abf30
BrunoAlan <alan.brunoab@gmail.com>
2024-09-07 10:24:22 -0300

    refactor: Update JiraTasks component to use useTasksHook
    The code changes in `JiraTasks.tsx` refactor the `JiraTasks` component to use the `useTasksHook` custom hook. This hook provides functionality for handling tasks in the JiraPage component, including adding tasks, handling drag and drop events, and managing the state of tasks. This refactor improves code organization and maintainability by centralizing the task-related logic in a separate hook.

--- src/components/jira/JiraTasks.tsx
+++ src/components/jira/JiraTasks.tsx
@@ -1,11 +1,8 @@
-import { DragEvent } from 'react';
 import { IoAddOutline, IoCheckmarkCircleOutline } from 'react-icons/io5';
 import type { Task, TaskStatus } from '../../interfaces';
 import SingleTask from './SingleTask';
-import { useTaskStore } from '../../stores/tast/task.store';
 import classNames from 'classnames';
-import { useState } from 'react';
-import Swal from 'sweetalert2';
+import useTasksHook from '../../hooks/useTasksHook';
 
 interface Props {
     title: string;
@@ -14,48 +11,14 @@ interface Props {
 }
 
 export const JiraTasks = ({ title, status, tasks }: Props) => {
-    const isDragging = useTaskStore((state) => !!state.draggingTaskId);
-    const onTaskDrop = useTaskStore((state) => state.onTaskDrop);
-    const addTask = useTaskStore((state) => state.addTask);
-
-    const [onDragOver, setOnDragOver] = useState(false);
-
-    const handleAddTask = async () => {
-        const { isConfirmed, value } = await Swal.fire({
-            title: 'Add Task',
-            inputLabel: 'Task Title',
-            input: 'text',
-            inputPlaceholder: 'Enter Task Title',
-            showCancelButton: true,
-            confirmButtonText: 'Add Task',
-            cancelButtonText: 'Cancel',
-            inputValidator: (value) => {
-                if (!value) {
-                    return 'Task Title is required!';
-                }
-            },
-        });
-        if (!isConfirmed) return;
-
-        addTask(value, status);
-    };
-
-    const handleDragOver = (event: DragEvent<HTMLDivElement>) => {
-        event.preventDefault();
-        setOnDragOver(true);
-    };
-
-    const handleDragLeave = (event: DragEvent<HTMLDivElement>) => {
-        event.preventDefault();
-        setOnDragOver(false);
-    };
-
-    const handleDrop = (event: DragEvent<HTMLDivElement>) => {
-        event.preventDefault();
-        setOnDragOver(false);
-        onTaskDrop(status);
-    };
-
+    const {
+        isDragging,
+        onDragOver,
+        handleAddTask,
+        handleDragOver,
+        handleDragLeave,
+        handleDrop,
+    } = useTasksHook({ status });
     return (
         <div
             onDragOver={handleDragOver}
