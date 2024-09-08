f0b392122c9e7e15e2ff629eceb079536348456e
BrunoAlan <alan.brunoab@gmail.com>
2024-09-08 16:46:55 -0300

    feat: Add checkStatus method to AuthService
    This commit adds the checkStatus method to the AuthService class in the auth.service.ts file. The checkStatus method makes an API call to '/auth/check-status' and returns the response data. If an error occurs, it logs the error and throws an error message. This addition allows for checking the authentication status in the application.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -30,4 +30,20 @@ export class AuthService {
             throw new Error('Unable to login');
         }
     };
+
+    static checkStatus = async (): Promise<LoginResponse> => {
+        try {
+            const { data } = await tesloApi.get<LoginResponse>(
+                '/auth/check-status'
+            );
+            return data;
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                console.log(error.response?.data);
+                throw new Error(error.response?.data);
+            }
+            console.log(error);
+            throw new Error('Unable to check status');
+        }
+    };
 }
